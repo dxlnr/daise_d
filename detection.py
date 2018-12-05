709c20da944099764014c50b068d48f27197bca4
frommwonderland <daniel.illner@outlook.de>
2018-12-05 21:08:04 +0100

    created detection file
    algorithm implementation

---
+++ detection.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dez 05 17:03:27 2018
+
+@author: DanielIllner
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+def compute_temporal_length():
+    return 0
+
+def compute_temporal_locality():
+    return 0
+
+
+def event_model_1():
+    return 0
+
+def event_model_2():
+    return 0
+
+def event_model_3():
+    return 0
+
+def model_loss():
+    return 0
+
+
+
+'''
+    forward_detection:
+        (1) Receive new sample Xn+1 and append it to X
+        (2) Update the clustering vector y and the clustering structure
+        (3) Check Loss(Mi, X, u, v) <= lambda for all u,v until it is satisfied
+'''
+def forward_detection(lambda):
+
+    while True:
+        self.X.append()
+
+        if loss <= lambda:
+            break
+
+    return 0
+
+'''
+    backward_reduction:
+        (1) Delete oldest sample X1 from the segment
+        (2) Update the clustering vector y and the clustering structure
+        (3) Check Loss(Mi, X, u, v) <= lambda for all u,v until it is not satisfied
+'''
+def backward_reduction():
+    return 0
+
+
+def detection():
+
+
+    while (True):
+
+    return 0
